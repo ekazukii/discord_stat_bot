df11874282b1560a8fc60f1543c7c4e0bf0f76b5
ekazuki <baptisteloison8400@gmail.com>
2020-10-14 11:53:11 +0200

    Add unit testing

---
+++ test/index.js
@@ -0,0 +1,17 @@
+//require('dotenv').config()
+//require("../discord.js")({dicord_token: process.env.DISCORD_TOKEN, xbox_key: process.env.XBOX_API_KEY});
+
+const assert = require('assert').strict;
+describe("Integration test", function() {
+    it("Should be able to calculate 1+1=2", function() {
+        assert.strictEqual(1+1, 2);
+    });
+
+    it("Should be true", function() {
+        assert.ok(true)
+    });
+
+    it("Should also be true", function() {
+        assert.ok(true)
+    });
+});
