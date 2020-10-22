0f11e2fa5eec96c723d122eeaa6785af6b90079e
ekazuki <baptisteloison8400@gmail.com>
2020-10-22 21:25:37 +0200

    Add command for comparing two players ($lol vs user1 user2)

--- models/lol.js
+++ models/lol.js
@@ -1,3 +1,4 @@
+"use strict";
 const request = require('request');
 
 module.exports = class LoLModel {
@@ -39,7 +40,7 @@ module.exports = class LoLModel {
                             response.rank = "UNRANKED"
                         }
 
-                        self.getMatchlist(aid, (body) => {
+                        self.getMatchlist(aid, 1, (body) => {
                             var gameId = body.matches[0].gameId;
 
                             self.getMatchInfo(gameId, (body) => {
@@ -53,7 +54,7 @@ module.exports = class LoLModel {
                                 response.match.kills = participant.stats.kills;
                                 response.match.deaths = participant.stats.deaths;
                                 response.match.assists = participant.stats.assists;
-                                response.match.cs = Math.round( (participant.stats.totalMinionsKilled + participant.stats.neutralMinionsKilledTeamJungle + participant.stats.neutralMinionsKilledEnemyJungle) / (body.gameDuration / 60) * 10) / 10;
+                                response.match.cs = self.getCSPerMinutes(participant.stats, body.gameDuration);
 
                                 console.log(response)
                                 callback(response)
@@ -71,6 +72,71 @@ module.exports = class LoLModel {
         });
     }
 
+    comparePlayers(options, callback) {
+        var user1 = options.user1;
+        var user2 = options.user2;
+        var self = this;
+        this.getSummonerByName(user1, (summ1) => {
+            self.getSummonerByName(user2, (summ2) => {
+                if(typeof summ1.id !== 'undefined' && typeof summ2.id !== 'undefined') {
+                    var aid1 = summ1.accountId;
+                    var aid2 = summ2.accountId;
+
+                    self.getClassicMatchlist(aid1, 5, (matchlist1) => {
+                        self.getClassicMatchlist(aid2, 5, (matchlist2) => {
+
+                            function getMatchlistScore(index, matchlist, score, aid, callback) {
+                                if(index <= matchlist1.length - 1) {
+                                    self.getMatchInfo(matchlist[index].gameId, (game) => {
+                                        var newScore = self.getMatchScore(game, aid);
+                                        for (let i = 0; i < score.length; i++) {
+                                            score[i] += newScore[i];
+                                        }
+                                        index++;
+                                        getMatchlistScore(index, matchlist, score, aid, callback);
+                                    });
+                                } else {
+                                    for (let i = 0; i < score.length; i++) {
+                                        score[i] = Math.round(score[i]);
+                                    }
+                                    callback(score)
+                                }
+                            }
+                            const reducer = (accumulator, currentValue) => accumulator + currentValue;
+                            getMatchlistScore(0, matchlist1, [0,0,0,0,0,0,0,0], aid1, (score1) => {
+                                getMatchlistScore(0, matchlist2, [0,0,0,0,0,0,0,0], aid2, (score2) => {
+                                    console.log(user1 + " : " + score1.reduce(reducer) + " - " + user2 + " : " + score2.reduce(reducer));
+                                    
+                                    callback({score1: score1, score2: score2});
+                                });
+                            });
+                            
+                            /*for (let i = 0; i < matchlist1.length; i++) {
+                                const match = matchlist1[i];
+                                score1 += self.getMatchScore(match);
+                            }
+                            
+                            for (let i = 0; i < matchlist2.length; i++) {
+                                const match = matchlist2[i];
+                                score2 += self.getMatchScore(match);
+                            }*/
+                        })
+                    })
+                } else {
+                    var response;
+                    response.error = true;
+                    response.error_desc = "user not found";
+                    callback(response);
+                }
+            })
+        })
+        // Get matchlist
+        // Choose only normal or ranked game
+        // Calculate score
+    }
+
+
+
     getChampName(champId) {
         for (const key in this.champJSON.data) {
             if(this.champJSON.data[key].key == champId) {
@@ -87,11 +153,56 @@ module.exports = class LoLModel {
         });
     }
     
-    getMatchlist(accountId, callback) {
-        request(`https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}?api_key=${this.apikey}`, { json: true }, (err, res, body) => {
+    getMatchlist(accountId, endIndex, callback) {
+        request(`https://euw1.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}?api_key=${this.apikey}&endIndex=${endIndex}`, { json: true }, (err, res, body) => {
             if (err) { return console.log(err); }
             callback(body);
         });
+    }
+
+    getClassicMatchlist(accountId, number, callback) {
+        var matchlist = [];
+        this.getMatchlist(accountId, number*5, (body) => {
+            var i = 0;
+            while(matchlist.length !== number && i !== body.matches.length-1) {
+                const match = body.matches[i];
+                if(match.queue === 400 || match.queue === 420 || match.queue === 430 || match.queue === 440) {
+                    matchlist.push(match);
+                }
+                i++;
+            }
+
+            callback(matchlist);
+        })
+    }
+
+    getMatchScore(match, accountId) {
+        var pid = this.getParticipantId(match, accountId);
+        var participant = this.getParticipant(match, pid);
+        var team = this.getTeam(match, participant);
+        var score = new Array();
+
+        if(team.win === "Win") {
+            score.push(20);
+        } else {
+            score.push(0);
+        }
+
+        score.push(participant.stats.kills * 3);
+        //console.log("kills: " + participant.stats.kills * 3 + " : "  + score)
+        score.push(participant.stats.deaths * -3);
+        //console.log("death: " + participant.stats.deaths * -3 + " : "  + score)
+        score.push(participant.stats.assists);
+        //console.log("assists: " + participant.stats.assists + " : "  + score)
+        score.push(this.getCSPerMinutes(participant.stats, match.gameDuration));
+        //console.log("cs: " + this.getCSPerMinutes(participant.stats, match.gameDuration) + " : "  + score);
+        score.push((participant.stats.timeCCingOthers * 4) / (match.gameDuration / 60));
+        //console.log("cc: " + (participant.stats.timeCCingOthers * 4) / (match.gameDuration / 60) + " : "  + score)
+        score.push(participant.stats.totalDamageDealtToChampions / ( (match.gameDuration * 125) / 60));
+        //console.log("damage: " + participant.stats.totalDamageDealtToChampions / ( (match.gameDuration * 125) / 60) + " : "  + score)
+        score.push(participant.stats.totalDamageTaken / ( (match.gameDuration * 150) / 60));
+        //console.log("tank: " + participant.stats.totalDamageTaken / ( (match.gameDuration * 150) / 60) + " : "  + score)
+        return score;
     }
 
     getMatchInfo(gameId, callback) {
@@ -117,5 +228,18 @@ module.exports = class LoLModel {
                 return player;
             }
         }
+    }
+
+    getTeam(game, participant) {
+        for (let i = 0; i < game.teams.length; i++) {
+            const element = game.teams[i];
+            if(game.teams[i].teamId === participant.teamId) {
+                return element;
+            }
+        }
+    }
+
+    getCSPerMinutes(stats, gameDuration) {
+        return Math.round( (stats.totalMinionsKilled + stats.neutralMinionsKilledTeamJungle + stats.neutralMinionsKilledEnemyJungle) / (gameDuration / 60) * 10) / 10;
     }
 }
