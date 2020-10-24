093aa4e42fcd1e470b81c59999dc907258033fb2
ekazuki <baptisteloison8400@gmail.com>
2020-10-24 15:46:08 +0200

    Add WynncraftModel unit test.

--- test/index.js
+++ test/index.js
@@ -7,10 +7,14 @@ var riotapi = process.env.RIOT_API;
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const LoLController = require("../controller/lol.js");
+const WynncraftController = require("../controller/wynncraft.js");
 const assert = require('assert').strict;
 const request = require("request");
 
-var lolController;
+const WynncraftModel = require("../models/wynncraft.js");
+const LoLModel = require("../models/lol.js");
+
+var lolController, wynncraftController;
 
 describe("Discord tests", function() {
     
@@ -18,44 +22,67 @@ describe("Discord tests", function() {
         client.on('ready', () => {
             client.user.setStatus('visible');
             lolController = new LoLController(client, riotapi);
+            wynncraftController = new WynncraftController(client)
             done();
         });
-      });
+    });
 
+    describe("Models UT", function() {
+        describe("League of Legends UT", function() {
+            var model = new LoLModel(riotapi);
 
-    describe("League of Legends UT", function() {
-        it("Should get user statistics", function(done) {
-            lolController.model.getUserStats({username: "ekazukii"}, (stats) => {
-                if(typeof stats.match !== "undefined") {
-                    done();
-                }
+            it("Should get user statistics", function(done) {
+                model.getUserStats({username: "ekazukii"}, (stats) => {
+                    if(typeof stats.match !== "undefined") {
+                        done();
+                    }
+                });
             });
+    
+            it("Should get current champion rotation", function(done) {
+                model.getChampionRotation((champList) => {
+                    if(champList.length === 15) done();
+                });
+            });
+    
+            it("Should compare ekazukii and ObstinateM", function(done) {
+                const reducer = (accumulator, currentValue) => accumulator + currentValue;
+                model.comparePlayers({user1: "ekazukii", user2: "ObstinateM"}, (stats) => {
+                    if(stats.score1.reduce(reducer) > 20 && stats.score2.reduce(reducer) > 20) {
+                        done();
+                    }
+                });
+            });
+    
+            it("Should get user creep score over last 5 games", function(done) {
+                lolController.model.getUserCS({username: "ekazukii", ngame: 5}, (stats) => {
+                    if(stats.cs.length === 5) {
+                        done();
+                    }
+                });
+            });
+    
+            
         });
 
-        it("Should get current champion rotation", function(done) {
-            lolController.model.getChampionRotation((champList) => {
-                if(champList.length === 15) done();
+        describe("Wynncraft UT", function() {
+            var model = new WynncraftModel()
+            it("Should get user statistics", function(done) {
+                model.getUserStats({username: "zefut"}, (stats) => {
+                    if(stats.mobsKilled > 1) {
+                        done();
+                    }
+                });
+            });
+
+            it("Should not found the user", function(done) {
+                model.getUserStats({username: "IDONTEXIST444552DJJDHDGZ"}, (stats) => {
+                    if(stats.error) {
+                        done();
+                    }
+                });
             });
         });
-
-        it("Should compare ekazukii and ObstinateM", function(done) {
-            const reducer = (accumulator, currentValue) => accumulator + currentValue;
-            lolController.model.comparePlayers({user1: "ekazukii", user2: "ObstinateM"}, (stats) => {
-                if(stats.score1.reduce(reducer) > 20 && stats.score2.reduce(reducer) > 20) {
-                    done();
-                }
-            });
-        });
-
-        it("Should get user creep score over last 5 games", function(done) {
-            lolController.model.getUserCS({username: "ekazukii", ngame: 5}, (stats) => {
-                if(stats.cs.length === 5) {
-                    done();
-                }
-            });
-        });
-
-        
     });
 });
 
