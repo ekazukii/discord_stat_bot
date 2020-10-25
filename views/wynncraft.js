ac0134224bd6a40ee62704a28be9329bd5013501
ekazuki <baptisteloison8400@gmail.com>
2020-10-25 15:53:55 +0100

    Add french and english lang files for wynncraft command.

--- views/wynncraft.js
+++ views/wynncraft.js
@@ -1,39 +1,42 @@
 const View = require("./view.js");
+const langJSON = require("./lang/en_EN.json")
+const language = langJSON.wynncraft;
+const error = langJSON.error;
 module.exports = class WynncraftView extends View {
-  constructor(bot) {
-    super(bot);
-  }
+    constructor(bot) {
+        super(bot);
+    }
 
-  showUserStats(stats, callback) {
-    var message = this.getEmbed()
-    message.embed.title = "Statistiques de "+ stats.username + " Sur Wynncraft";
-    message.embed.fields.push({
-      name: 'Type de la classe',
-      value: stats.className
-    }, {
-      name: "Level de la classe",
-      value: stats.classLevel
-    }, {
-      name: "Nombre de dongeons compltétés",
-      value: stats.completedDungeons
-    }, {
-      name: "Nombre de quêtes accomplies",
-      value: stats.completedQuests
-    }, {
-      name: "Nombre de mob tués",
-      value: stats.mobsKilled
-    });
-    callback(message)
-  }
+    showUserStats(stats, callback) {
+        var message = this.getEmbed()
+        message.embed.title = language.statsTitle.replace("{arg1}", stats.username);
+        message.embed.fields.push({
+            name: language.statsFieldClassName,
+            value: stats.className
+            }, {
+            name: language.statsFieldLevelName,
+            value: stats.classLevel
+            }, {
+            name: language.statsFieldDungeonsName,
+            value: stats.completedDungeons
+            }, {
+            name: language.statsFieldQuestsName,
+            value: stats.completedQuests
+            }, {
+            name: language.statsFieldMobsName,
+            value: stats.mobsKilled
+        });
+        callback(message)
+    }
 
-  printError(stats, callback) {
-    var message = this.getEmbedError()
-    message.embed.title = "Erreur";
-    message.embed.fields.push({
-      name: 'Erreur lors de la requete',
-      value: "L'utilisaeur n'a pas été trouvé, réessayez avec l'uuid"
-    });
+    printError(stats, callback) {
+        var message = this.getEmbedError()
+        message.embed.title = error.errorTitle;
+        message.embed.fields.push({
+            name: error.errorFieldName,
+            value: error.errorFieldValue
+        });
 
-    callback(message);
-  }
+        callback(message);
+    }
 }
