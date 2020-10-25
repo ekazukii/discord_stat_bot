537b5f2831a298e2a6ad3d2d7f36abdbd4e5d6a4
ekazuki <baptisteloison8400@gmail.com>
2020-10-25 15:08:14 +0100

    Add french and english lang files for hivemc command.

--- views/hivemc.js
+++ views/hivemc.js
@@ -1,35 +1,39 @@
 const View = require("./view.js");
+const langJSON = require("./lang/fr_FR.json")
+const language = langJSON.hivemc;
+const error = langJSON.error;
+
 module.exports = class HivemcView extends View {
-  constructor(bot) {
-    super(bot)
-  }
+    constructor(bot) {
+        super(bot)
+    }
 
-  showUserStats(stats, callback) {
-    var message = this.getEmbed()
-    message.embed.title = "Statistiques de "+ stats.username + " Sur HiveMC"
-    message.embed.fields.push({
-      name: 'Nombre de victoires en Hide and Seek',
-      value: stats.hide
-    }, {
-      name: "Nombre de victoires en gravity",
-      value: stats.grav
-    }, {
-      name: "Nombre de victoires en blockparty",
-      value: stats.blockparty
-    }, {
-      name: "Nombre de victoires en deathrun",
-      value: stats.deathrun
-    });
-    callback(message);
-  }
+    showUserStats(stats, callback) {
+        var message = this.getEmbed()
+        message.embed.title = language.statsTitle.replace("{arg1}", stats.username);
+        message.embed.fields.push({
+            name: language.statsHideAndSeek,
+            value: stats.hide
+        }, {
+            name: language.statsGravity,
+            value: stats.grav
+        }, {
+            name: language.statsBlockparty,
+            value: stats.blockparty
+        }, {
+            name: language.statsDeathrun,
+            value: stats.deathrun
+        });
+        callback(message);
+    }
 
-  printError(stats, callback) {
-    var message = this.getEmbedError()
-    message.embed.title = "Erreur"
-    message.embed.fields.push({
-      name: 'Erreur lors de la requete',
-      value: "L'utilisaeur n'a pas été trouvé"
-    });
-    callback(message);
-  }
+    printError(stats, callback) {
+        var message = this.getEmbedError()
+        message.embed.title = error.errorTitle
+        message.embed.fields.push({
+            name: error.errorFieldName,
+            value: error.errorFieldValue
+        });
+        callback(message);
+    }
 }
