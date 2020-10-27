509d1b958e062992a5b0bb9bb77e4dc16cab7454
ekazuki <baptisteloison8400@gmail.com>
2020-10-27 14:25:32 +0100

    Add documentation of lang command.

--- controller/lang.js
+++ controller/lang.js
@@ -1,17 +1,38 @@
 const LangView = require("../views/lang.js");
 const LangModel = require("../models/lang.js");
 
-module.exports = class HivemcController {
+/** Controller for Lang command */
+class LangController {
+
+    /**
+     * @todo Instanciate model and view in constructor.
+     * @param {DiscordClient} client 
+     * @param {Object} db - {@link https://www.npmjs.com/package/sqlite3|SQLITE DB Object} 
+     */
     constructor(client, db) {
         this.client = client;
         this.model = new LangModel(db);
         this.view = new LangView(client.user);
     }
 
+    /**
+     * Process command with command arguments (in this case always do changeLang only command)
+     * @param {Array<string>} args - Command arguments
+     * @param {string} lang - Current language of the bot
+     * @param {number} sid - Discord server identifier
+     * @param {messageCallback} callback 
+     */
     command(args, lang, sid, callback) {
         this.changeLang(args[0], lang, sid, callback);
     }
 
+    /**
+     * Ask the model to fetch statistics and send them to the view, then callback the message created in view
+     * @param {string} newLang - New language of the bot 
+     * @param {string} lang - Current language of the bot
+     * @param {number} sid - Discord server identifier  
+     * @param {messageCallback} callback 
+     */
     changeLang(newLang, lang, sid, callback) {
         var self = this;
         this.model.changeLang(newLang, sid, function(err) {
@@ -28,3 +49,5 @@ module.exports = class HivemcController {
         });
     }
 }
+
+module.exports = LangController;
