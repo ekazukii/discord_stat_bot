ef8eee681d9c55d08233b4f13b060ce0a9a0291c
Baptiste Loison <baptisteloison8400@gmail.com>
2020-11-17 15:40:09 +0100

    Optimise Hivemc command

--- test/dev.js
+++ test/dev.js
@@ -1,9 +1,2 @@
 require('dotenv').config();
-
-const MojangModel = require('../build/models/mojang')
-var model = new MojangModel.MojangModel();
-model.getUUIDByUsername("ekazukiJHYTRYUI", (id) => {
-    console.log(id)
-})
-
-//require("../build/discord")({dicord_token: process.env.DISCORD_TOKEN, xbox_key: process.env.XBOX_API_KEY, riotapi: process.env.RIOT_API, hypixelapi: process.env.HYPIXEL_API, cocapi: process.env.COC_API, csgoapi: process.env.CSGO_API});
+require("../build/discord")({dicord_token: process.env.DISCORD_TOKEN, xbox_key: process.env.XBOX_API_KEY, riotapi: process.env.RIOT_API, hypixelapi: process.env.HYPIXEL_API, cocapi: process.env.COC_API, csgoapi: process.env.CSGO_API});
